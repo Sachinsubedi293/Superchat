a1027f1253cb8da4060f7956d0299b43521ea71c
Sachin Subedi <80297252+Sachinsubedi293@users.noreply.github.com>
2024-10-19 21:45:13 +0545

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -8,14 +8,7 @@ import { getAuth } from "@firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAXqri2mmwxU5yAsbihBJfXbpOr9WjnuAY",
-  authDomain: "webapp-653ea.firebaseapp.com",
-  databaseURL: "https://webapp-653ea-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "webapp-653ea",
-  storageBucket: "webapp-653ea.appspot.com",
-  messagingSenderId: "566890173783",
-  appId: "1:566890173783:web:b239dee412325d0515da12",
-  measurementId: "G-VY08ZKEMLY"
+
 };
 
 // Initialize Firebase
